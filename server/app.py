879575233c10cb44a7912cc079bdf9e0de3e203c
valeria caliguire <caliguireval@gmail.com>
2023-06-14 15:01:45 -0600

    completed, use as template

--- server/app.py
+++ server/app.py
@@ -30,17 +30,130 @@ def bakeries():
     )
     return response
 
-@app.route('/bakeries/<int:id>')
+
+#DELIVERABLE 1:
+#Define a POST block inside of a /baked_goods route
+#creates a new baked good in the database and 
+#returns its data as JSON. The request will send data in a form.
+#add method = ['POST'] bd GET is the defualt
+@app.route('/baked_goods', methods=['POST', 'GET'])
+def baked_goods():
+    baked_goods = BakedGood.query.all()
+    if request.method == 'GET':
+        bg_serialized = [bg.to_dict() for bg in baked_goods]
+
+
+        response = make_response(
+            jsonify(bg_serialized),
+            200
+        )
+
+        return response
+    elif request.method == 'POST':
+        #THIS IS AN INSTANCE OF THE CLASS BAKEDBOOD
+        new_baked_good = BakedGood(
+            name=request.form.get('name'),
+            price=request.form.get('price'),
+            bakery_id=request.form.get('bakery_id'),
+        )
+
+        db.session.add(new_baked_good)
+        db.session.commit()
+
+        new_baked_good_dict = new_baked_good.to_dict()
+
+        response = make_response(
+            jsonify(new_baked_good_dict),
+            201
+        )
+
+        return response
+
+
+#DELIVERABLE 2
+#Define a PATCH block inside of the /bakeries/<int:id> route 
+#that updates the name of the bakery in the database 
+#and returns its data as JSON. As with the previous POST block, 
+#the request will send data in a form. 
+#The form does not need to include values 
+#for all of the bakery's attributes.
+@app.route('/bakeries/<int:id>', methods=['PATCH', 'GET'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
+    if request.method == 'GET':
+        bakery_serialized = bakery.to_dict()
 
-    response = make_response(
-        bakery_serialized,
-        200
-    )
-    return response
+        response = make_response(
+            bakery_serialized,
+            200
+        )
+        return response
+    elif request.method == 'PATCH':
+        #for each attribute you want to change in the request.form
+        #setattr() param1=query variable, param2=attribute to be changed
+        #param3=request.form.get(attribute)
+        #for attr in request.form:
+                #setattr(review, attr, request.form.get(attr))
+        for name in request.form:
+                setattr(bakery, name, request.form.get(name))
+        db.session.add(bakery)
+        db.session.commit()
+
+        #TURN SQL/PYTHON CODE TO JSON CODE
+        bakery_dict = bakery.to_dict()
+
+        response = make_response(
+            jsonify(bakery_dict),
+            200
+        )
+
+        return response
+
+
+#DELIVERABLE 3
+#Define a DELETE block inside of a /baked_goods/<int:id> route 
+#that deletes the baked good from the database and 
+#returns a JSON message 
+#confirming that the record was successfully deleted.
+@app.route('/baked_goods/<int:id>', methods= ['DELETE', 'GET'])
+def delete_goods(id):
+    #you are getting ONE ITEM, so name is SINGULAR
+    good = BakedGood.query.filter(BakedGood.id == id).first()
+    if request.method == 'GET':
+        #DONT need a list so leave it OUT of brackets
+        #the query variable is ONE ITEM, so you only need the ONE
+        #item to turn into a dictionary
+        goods_serialized = good.to_dict()
+
+        response = make_response(
+            jsonify(goods_serialized),
+            200
+        )
+        return response
+    
+    elif request.method == 'DELETE':
+        #use the delete method on the thing you want to be deleted
+        #in this case, we did a query to access ONE good
+        #pass that variable...good...as the argument
+        db.session.delete(good)
+        db.session.commit()
+
+        #need to send a response back to say that it was successful
+        response_body = {
+                "delete_successful": True,
+                "message": "Review deleted."    
+            }
+
+        response = make_response(
+            jsonify(response_body),
+            200
+        )
+
+        return response
+
+
+
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
